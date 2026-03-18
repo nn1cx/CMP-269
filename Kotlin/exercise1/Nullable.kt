fb8292262afd9319280d182c6dbe49cbc7566803
nn1cx <148999600+nn1cx@users.noreply.github.com>
2026-03-17 20:21:09 -0400

    Refactor nullable middleName handling in main

--- Kotlin/exercise1/Nullable.kt
+++ Kotlin/exercise1/Nullable.kt
@@ -1,7 +1,6 @@
-fun main() {
+fun main (){
     val studentName: String = "Judd"
     val middleName: String? = null
-    var noMiddleName = middleName?: "No Middle Name"
 
-    println("Welcome, $studentName $noMiddleName")
+    println("Welcome, $studentName " + (middleName ?: "No Middle Name"))
 }
