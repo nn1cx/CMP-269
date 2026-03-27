d7e7687e58ec4f3da394ee6e4c3838d1d797764c
nn1cx <148999600+nn1cx@users.noreply.github.com>
2026-03-26 21:13:40 -0400

    Add StaticContent.kt for serving static resources

---
+++ Kotlin/exercise3/StaticContent.kt
@@ -0,0 +1,20 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.http.content.*
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        staticResources("/static", "static")
+    }
+}
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        configureRouting()
+    }.start(wait = true)
+}
